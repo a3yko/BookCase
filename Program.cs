cd2e14bd13523252be920c7c8e6af2725a3938f0
Adrian Atanasov <adriansatanasov@gmail.com>
2021-02-25 23:11:22 -0600

    github add

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BookCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book myBook = new Book("Ready Player One", "Ernest Cline", 19.99, "978-0307887443");
+
+           Console.WriteLine(myBook.printInformation());
+        }
+    }
+}
